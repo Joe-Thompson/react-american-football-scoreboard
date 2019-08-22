55cb6e9d20a6fb318658950e0204d3f05829b6fa
Joe Thompson <joe@pop-os.localdomain>
2019-08-21 22:07:30 -0400

    MVP completed, still working on changing possesion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
+  const [homeScore, updateHomeScore] = useState(0);
+  const [awayScore, updateAwayScore] = useState(0);
+  
   return (
     <div className="container">
       <section className="scoreboard">
+        <button onClick = {this.toggleDefense} className = "possesion">Change Possesion</button>
         <div className="topRow">
           <div className="home">
             <h2 className="home__name">Lions</h2>
+            
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
+            <p className = {`${defense ? "" : defense}`}><i class="fas fa-football-ball"></i></p>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
+            <p className = {`defense ${defense ? "" : defense}`}><i class="fas fa-football-ball"></i></p>
           </div>
         </div>
         <BottomRow />
@@ -28,12 +35,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
+          <button onClick = { () => updateHomeScore(homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick = { () => updateHomeScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+        </div>_
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick = { () => updateAwayScore(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick = { () => updateAwayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
